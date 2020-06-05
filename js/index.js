4b52db3ddfea1df6860ef99f07a747f4ee50d89e
pavel <browser091@gmail.com>
2020-06-05 14:49:58 +0300

    add right side bar and @media

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu-sidebar");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
